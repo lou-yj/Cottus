6ee506563975659e0126c14609d73ccc52bff6e9
louyj <ksfgitL_yj>
2020-03-25 16:58:09 +0800

    shell 响应ctl+c

--- rhttptunnel-client/src/main/java/com/louyj/rhttptunnel/client/worker/ControlCommand.java
+++ rhttptunnel-client/src/main/java/com/louyj/rhttptunnel/client/worker/ControlCommand.java
@@ -6,7 +6,14 @@ import static com.louyj.rhttptunnel.model.http.Endpoints.CLIENT_EXCHANGE;
 import java.util.Scanner;
 import java.util.UUID;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.jline.reader.EndOfFileException;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.UserInterruptException;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.Availability;
 import org.springframework.shell.standard.ShellComponent;
@@ -101,31 +108,42 @@ public class ControlCommand {
 		if (StringUtils.equals(resp, "Worker ready")) {
 			System.out.println("OK, Worker ready");
 			System.out.println("WARNNING YOU NOW IN INTERACTIVE SHELL MODE!!!");
-			System.out.print("shell:> ");
 		} else {
 			System.out.println(resp);
 			return Status.FAILED;
 		}
-		while (true) {
-			line = sc.nextLine();
-			if (StringUtils.equals(StringUtils.trim(line), "exit")) {
-				ShellEndMessage shellEndMessage = new ShellEndMessage(CLIENT, exchangeId);
-				BaseMessage endMessage = messageExchanger.jsonPost(CLIENT_EXCHANGE, shellEndMessage);
-				String echo = messagePoller.pollExchangeMessage(endMessage);
-				System.out.print(echo);
-				break;
+		Terminal terminal = TerminalBuilder.builder().system(true).build();
+		LineReader lineReader = LineReaderBuilder.builder().terminal(terminal).build();
+		String prompt = "shell:> ";
+		try {
+			while (true) {
+				line = lineReader.readLine(prompt);
+				if (StringUtils.equals(StringUtils.trim(line), "exit")) {
+					ShellEndMessage shellEndMessage = new ShellEndMessage(CLIENT, exchangeId);
+					BaseMessage endMessage = messageExchanger.jsonPost(CLIENT_EXCHANGE, shellEndMessage);
+					String echo = messagePoller.pollExchangeMessage(endMessage);
+					System.out.print(echo);
+					break;
+				}
+				if (StringUtils.endsWith(line, "\\")) {
+					System.out.println("Multi line command current not support!");
+				} else if (StringUtils.isNotBlank(line)) {
+					ShellMessage shellMessage = new ShellMessage(CLIENT, exchangeId);
+					shellMessage.setMessage(line);
+					BaseMessage post = messageExchanger.jsonPost(CLIENT_EXCHANGE, shellMessage);
+					String echo = messagePoller.pollExchangeMessage(post);
+					System.out.print(echo);
+				}
 			}
-			if (StringUtils.endsWith(line, "\\")) {
-				System.out.println("Multi line command current not support!");
-			} else if (StringUtils.isNotBlank(line)) {
-				ShellMessage shellMessage = new ShellMessage(CLIENT, exchangeId);
-				shellMessage.setMessage(line);
-				BaseMessage post = messageExchanger.jsonPost(CLIENT_EXCHANGE, shellMessage);
-				String echo = messagePoller.pollExchangeMessage(post);
-				System.out.print(echo);
-			}
-			System.out.print("shell:> ");
+		} catch (UserInterruptException e) {
+			System.out.println("Try to ask worker work exit interactive shell mode");
+			ShellEndMessage shellEndMessage = new ShellEndMessage(CLIENT, exchangeId);
+			BaseMessage endMessage = messageExchanger.jsonPost(CLIENT_EXCHANGE, shellEndMessage);
+			String echo = messagePoller.pollExchangeMessage(endMessage);
+			System.out.print(echo);
+		} catch (EndOfFileException e) {
 		}
+		IOUtils.closeQuietly(terminal);
 		return "\nExit interactive shell mode";
 	}
 
