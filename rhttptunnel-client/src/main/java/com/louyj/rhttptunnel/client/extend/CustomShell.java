8e6d1bda8f80379f197cb34f59b8d42c1d4535b4
Louyj <Louyj>
2020-07-21 18:43:22 +0800

    add check permission entry

--- rhttptunnel-client/src/main/java/com/louyj/rhttptunnel/client/extend/CustomShell.java
+++ rhttptunnel-client/src/main/java/com/louyj/rhttptunnel/client/extend/CustomShell.java
@@ -1,12 +1,32 @@
 package com.louyj.rhttptunnel.client.extend;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.shell.Availability;
+import org.springframework.shell.CommandNotCurrentlyAvailable;
+import org.springframework.shell.CommandNotFound;
 import org.springframework.shell.ExitRequest;
 import org.springframework.shell.Input;
 import org.springframework.shell.InputProvider;
+import org.springframework.shell.MethodTarget;
+import org.springframework.shell.ParameterResolver;
+import org.springframework.shell.ParameterValidationException;
 import org.springframework.shell.ResultHandler;
 import org.springframework.shell.Shell;
+import org.springframework.shell.Utils;
+import org.springframework.util.ReflectionUtils;
+
+import com.louyj.rhttptunnel.client.ClientSession;
 
 /**
  *
@@ -19,10 +39,13 @@ import org.springframework.shell.Shell;
 public class CustomShell extends Shell {
 
 	private final ResultHandler syncHandler;
+	private Validator validatorx;
+	private ClientSession clientSession;
 
-	public CustomShell(ResultHandler resultHandler) {
+	public CustomShell(ResultHandler resultHandler, ClientSession clientSession) {
 		super(resultHandler);
 		this.syncHandler = resultHandler;
+		this.clientSession = clientSession;
 	}
 
 	@SuppressWarnings("unchecked")
@@ -42,7 +65,6 @@ public class CustomShell extends Shell {
 			if (input == null) {
 				break;
 			}
-			checkPermission(input);
 			result = evaluate(input);
 			if (result != NO_INPUT && !(result instanceof ExitRequest)) {
 				syncHandler.handleResult(result);
@@ -50,8 +72,99 @@ public class CustomShell extends Shell {
 		}
 	}
 
-	public void checkPermission(Input input) {
-		// todo
+	public Object evaluate(Input input) {
+		if (noInput(input)) {
+			return NO_INPUT;
+		}
+
+		String line = input.words().stream().collect(Collectors.joining(" ")).trim();
+		String command = findLongestCommand(line);
+
+		List<String> words = input.words();
+		if (command != null) {
+			MethodTarget methodTarget = methodTargets.get(command);
+			Availability availability = methodTarget.getAvailability();
+			if (availability.isAvailable()) {
+				List<String> wordsForArgs = wordsForArguments(command, words);
+				Method method = methodTarget.getMethod();
+				checkPermission(method, command);
+				try {
+					Object[] args = resolveArgs(method, wordsForArgs);
+					validateArgs(args, methodTarget);
+					return ReflectionUtils.invokeMethod(method, methodTarget.getBean(), args);
+				} catch (Exception e) {
+					return e;
+				}
+			} else {
+				return new CommandNotCurrentlyAvailable(command, availability);
+			}
+		} else {
+			return new CommandNotFound(words);
+		}
+	}
+
+	public void checkPermission(Method method, String command) {
+		System.out.println(String.format("command %s method %s", command, method.getName()));
+	}
+
+	private boolean noInput(Input input) {
+		return input.words().isEmpty() || (input.words().size() == 1 && input.words().get(0).trim().isEmpty())
+				|| (input.words().iterator().next().matches("\\s*//.*"));
+	}
+
+	private String findLongestCommand(String prefix) {
+		String result = methodTargets.keySet().stream()
+				.filter(command -> prefix.equals(command) || prefix.startsWith(command + " "))
+				.reduce("", (c1, c2) -> c1.length() > c2.length() ? c1 : c2);
+		return "".equals(result) ? null : result;
+	}
+
+	private List<String> wordsForArguments(String command, List<String> words) {
+		int wordsUsedForCommandKey = command.split(" ").length;
+		List<String> args = words.subList(wordsUsedForCommandKey, words.size());
+		int last = args.size() - 1;
+		if (last >= 0 && "".equals(args.get(last))) {
+			args.remove(last);
+		}
+		return args;
+	}
+
+	private Object[] resolveArgs(Method method, List<String> wordsForArgs) {
+		List<MethodParameter> parameters = Utils.createMethodParameters(method).collect(Collectors.toList());
+		Object[] args = new Object[parameters.size()];
+		Arrays.fill(args, UNRESOLVED);
+		for (ParameterResolver resolver : parameterResolvers) {
+			for (int argIndex = 0; argIndex < args.length; argIndex++) {
+				MethodParameter parameter = parameters.get(argIndex);
+				if (args[argIndex] == UNRESOLVED && resolver.supports(parameter)) {
+					args[argIndex] = resolver.resolve(parameter, wordsForArgs).resolvedValue();
+				}
+			}
+		}
+		return args;
+	}
+
+	private void validateArgs(Object[] args, MethodTarget methodTarget) {
+		for (int i = 0; i < args.length; i++) {
+			if (args[i] == UNRESOLVED) {
+				MethodParameter methodParameter = Utils.createMethodParameter(methodTarget.getMethod(), i);
+				throw new IllegalStateException("Could not resolve " + methodParameter);
+			}
+		}
+		if (validatorx == null) {
+			try {
+				Field declaredField = Shell.class.getDeclaredField("validator");
+				declaredField.setAccessible(true);
+				validatorx = (Validator) declaredField.get(this);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+		Set<ConstraintViolation<Object>> constraintViolations = validatorx.forExecutables()
+				.validateParameters(methodTarget.getBean(), methodTarget.getMethod(), args);
+		if (constraintViolations.size() > 0) {
+			throw new ParameterValidationException(constraintViolations, methodTarget);
+		}
 	}
 
 }
--- rhttptunnel-client/src/main/java/com/louyj/rhttptunnel/client/extend/CustomSpringShellConfiguration.java
+++ rhttptunnel-client/src/main/java/com/louyj/rhttptunnel/client/extend/CustomSpringShellConfiguration.java
@@ -1,5 +1,6 @@
 package com.louyj.rhttptunnel.client.extend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,8 @@ import org.springframework.shell.ResultHandler;
 import org.springframework.shell.Shell;
 import org.springframework.shell.SpringShellAutoConfiguration;
 import org.springframework.shell.result.ResultHandlerConfig;
+
+import com.louyj.rhttptunnel.client.ClientSession;
 
 /**
  *
@@ -21,9 +24,12 @@ import org.springframework.shell.result.ResultHandlerConfig;
 @Import(ResultHandlerConfig.class)
 public class CustomSpringShellConfiguration extends SpringShellAutoConfiguration {
 
+	@Autowired
+	private ClientSession clientSession;
+
 	@Bean
 	public Shell shell(@Qualifier("main") ResultHandler resultHandler) {
-		return new CustomShell(resultHandler);
+		return new CustomShell(resultHandler, clientSession);
 	}
 
 }
